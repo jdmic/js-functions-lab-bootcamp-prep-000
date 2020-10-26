63b3899ed0c3fa50f938169aeb3b8d15055a334f
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 15:16:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+
+function happyHolidays(){
+  return `Happy holidays ${name}!`;
+}
+
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days){
+  
+}
